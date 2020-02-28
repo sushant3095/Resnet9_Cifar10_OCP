5678d28b3dfbcff79f8529e6f155a3f479d9a32a
sushant3095 <sushantbhat30@gmail.com>
2020-02-28 23:07:10 +0530

    Update optim_scheduler.py

--- optim_scheduler/optim_scheduler.py
+++ optim_scheduler/optim_scheduler.py
@@ -19,6 +19,6 @@ class OptSchedule():
 
     def __init__(self, batches_per_epoch):
         self.lr_schedule = lambda t: np.interp([t], [0, (EPOCHS+1)//5, EPOCHS], [0, LEARNING_RATE, 0])[0]
-        self.global_step = tf.train.get_or_create_global_step()
+        self.global_step = tf.compat.v1.train.get_or_create_global_step()
         self.lr_func = lambda: self.lr_schedule(self.global_step/batches_per_epoch)/BATCH_SIZE
-        self.opt = tf.train.MomentumOptimizer(self.lr_func, momentum=MOMENTUM, use_nesterov=True)
+        self.opt = tf.compat.v1.train.MomentumOptimizer(self.lr_func, momentum=MOMENTUM, use_nesterov=True)
